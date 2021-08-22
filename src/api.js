5929d30d18e4b49cc6333cd3b5aed51e06f1cb71
BuildTools <unconfigured@null.spigotmc.org>
2021-08-23 00:30:25 +0300

    Project

---
+++ src/api.js
@@ -0,0 +1,43 @@
+//Base url
+const base_url = "https://api.rawg.io/api/";
+const apiKey = "4f7589513013449ab05c88f510e0bbf1";
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+const popular_games = `games?key=${apiKey}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?key=${apiKey}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const new_games = `games?key=${apiKey}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
+
+export const popularGamesURL = () => `${base_url}${popular_games}`;
+export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
+export const newGamesURL = () => `${base_url}${new_games}`;
+
+//GAME DETAILS
+
+export const gameDetailsURL = (game_id) =>
+  `${base_url}games/${game_id}?key=${apiKey}`;
+export const gameScreenshotURL = (game_id) =>
+  `${base_url}games/${game_id}/screenshots?key=${apiKey}`;
+
+export const searchGameURL = (game_name) =>
+  `${base_url}games?key=${apiKey}&search=${game_name}&page_size=9`;
